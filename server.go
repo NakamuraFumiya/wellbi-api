b1ff43971602df8bee36f22d1f5ed789cb0e51d6
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2022-09-13 10:35:03 +0900

    createPost関数を追加

--- server.go
+++ server.go
@@ -19,30 +19,35 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Connecting to a Database
-	dsn := "twitter_user:password@tcp(127.0.0.1:3306)/twitter?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
+	db := connectDB()
 	// Migrate the schema
 	db.AutoMigrate(&model.Post{})
-
-	// // Create
-	// db.Create(&model.Post{From: "Fumiya Nakamura", Message: "Hello!"})
 
 	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, this is a Twitter clone!")
 	})
-	e.POST("/posts", func(c echo.Context) error {
-		message := c.FormValue("message")
-		if message == "" {
-			return &echo.HTTPError{Code: http.StatusBadRequest, Message: "invalid to or message fields"}
-		}
-		db.Create(&model.Post{Message: message})
-		return nil
-	})
+	e.POST("/posts", createPost)
 
 	// Start Server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func connectDB() *gorm.DB {
+	dsn := "twitter_user:password@tcp(127.0.0.1:3306)/twitter?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
+
+func createPost(c echo.Context) error {
+	db := connectDB()
+	message := c.FormValue("message")
+	if message == "" {
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: "invalid to or message fields"}
+	}
+	db.Create(&model.Post{Message: message})
+	return nil
 }
