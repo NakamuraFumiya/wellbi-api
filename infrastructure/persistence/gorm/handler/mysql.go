508503ca9a5cc2346b2fc2928ed0962394e94b64
NakamuraFumiya <nakamura-fumiya@pla-cole.co>
2023-02-18 17:02:29 +0900

    tweak

--- infrastructure/persistence/gorm/handler/mysql.go
+++ infrastructure/persistence/gorm/handler/mysql.go
@@ -32,3 +32,16 @@ func (handler *Handler) DB() *gorm.DB {
 	}
 	return db
 }
+
+func ConnectDB() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("APP_USER"), os.Getenv("APP_PASSWORD"), os.Getenv("APP_DB_PORT"), os.Getenv("APP_DATABASE"))
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
